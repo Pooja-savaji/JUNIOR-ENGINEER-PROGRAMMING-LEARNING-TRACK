6c7a06dfb7f3c67a25e95765c5296267f2bdc1e9
Pooja-savaji <poojasavaji2005@gmail.com>
2026-09-21 12:52:57 +0530

    Add Shopping Bill calculator script

---
+++ 01-python-fundamentals/1.1-learned/01-syntax-and-execution/06 .Shopping Bill.py
@@ -0,0 +1,5 @@
+item1 = float(input("Enter item 1 price: "))
+item2 = float(input("Enter item 2 price: "))
+item3 = float(input("Enter item 3 price: "))
+total = item1 + item2 + item3
+print("Total Bill:", total)
