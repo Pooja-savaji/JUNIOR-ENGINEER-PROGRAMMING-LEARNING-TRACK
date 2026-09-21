8ef97f61730f80d01abc489a6fffa1c9356f9bb3
Pooja-savaji <poojasavaji2005@gmail.com>
2026-09-21 12:59:22 +0530

    Add grade calculator based on user input marks

---
+++ 01-python-fundamentals/1.1-learned/01-syntax-and-execution/09.Grade Calculator.py
@@ -0,0 +1,12 @@
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade:", grade)
