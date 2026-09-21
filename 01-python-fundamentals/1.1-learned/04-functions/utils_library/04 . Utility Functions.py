e1c82f82b4f1d2d2e5e93820a3e8dc5dc570bf7a
Pooja-savaji <poojasavaji2005@gmail.com>
2026-09-21 14:55:14 +0530

    Add utility functions for arithmetic operations

---
+++ 01-python-fundamentals/1.1-learned/04-functions/utils_library/04 . Utility Functions.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+def maximum(numbers):
+    return max(numbers)
+
+numbers = [10, 20, 30]
+
+print("Sum:", add(10, 20))
+print("Average:", average(numbers))
+print("Maximum:", maximum(numbers))
