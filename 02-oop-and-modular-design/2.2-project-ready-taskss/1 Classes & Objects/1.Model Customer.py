6479d0620f00f53ffafdc5998dba3e9a87127a36
Pooja-savaji <poojasavaji2005@gmail.com>
2026-09-22 10:39:01 +0530

    Add Customer class with name and email attributes

---
+++ 02-oop-and-modular-design/2.2-project-ready-taskss/1 Classes & Objects/1.Model Customer.py
@@ -0,0 +1,10 @@
+class Customer:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+customer = Customer("Pooja", "pooja@gmail.com")
+
+print(customer.name)
+print(customer.email)
